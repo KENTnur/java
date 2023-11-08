8600b6b8e38c0a322d60208e99e6f302fce35e6b
thqu1et <askar.abylkhaiyrov.00@gmail.com>
2023-11-08 13:03:04 +0600

    New changes

--- src/main/java/com/education/projectteam/controllers/MainController.java
+++ src/main/java/com/education/projectteam/controllers/MainController.java
@@ -4,15 +4,5 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
-    @GetMapping("/")
-    public String Home(Model model) {
-        model.addAttribute("title", "Home page");
-        return "home";
-    }
 
-    @GetMapping("/about")
-    public String About(Model model) {
-        model.addAttribute("title", "About page");
-        return "about";
-    }
 }
