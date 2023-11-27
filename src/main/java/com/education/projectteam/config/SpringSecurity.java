9d002f861e5aba255c0537b3f850e98d483b5fd0
scity <mukhitkudaibergenov1@gmail.com>
2023-11-27 16:30:37 +0600

    adding my edu

--- src/main/java/com/education/projectteam/config/SpringSecurity.java
+++ src/main/java/com/education/projectteam/config/SpringSecurity.java
@@ -34,6 +34,7 @@ public class SpringSecurity {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers("/register/**").permitAll()
+                                .requestMatchers("/MyEducation").permitAll()
                                 .requestMatchers("/main").permitAll()
                                 .requestMatchers("/static/**").permitAll()
                                 .requestMatchers("/userProfile/{id}").permitAll()
@@ -44,7 +45,7 @@ public class SpringSecurity {
                                 .loginPage("/login")
                                 .loginProcessingUrl("/login")
                                 .defaultSuccessUrl("/main")
-                                .defaultSuccessUrl("/userProfile/{id}/edit")
+//
                                 .permitAll()
                 ).logout(
                         logout -> logout
