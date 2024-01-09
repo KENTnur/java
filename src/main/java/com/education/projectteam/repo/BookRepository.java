fd7409b1429df104fc8ec33189e430546a8984e8
Kudaibergenov <mukhitkudaibergenov1@gmail.com>
2024-01-09 14:46:46 +0600

    Mukhit Mal

--- src/main/java/com/education/projectteam/repo/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.education.projectteam.repo;
-import com.education.projectteam.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book,Long>{
-    Book findByName(String name);
-}
